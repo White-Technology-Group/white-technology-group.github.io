4df34372f67a74d9c3fc7a4b09e3fec3db965706
Zack White <4070869+ZackWhiteIT@users.noreply.github.com>
2022-03-25 21:59:17 -0400

    Add sample template

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,3 +1,5 @@
+import time
+
 AUTHOR = 'Zack White'
 SITENAME = 'White Technology Group'
 SITEURL = ''
@@ -6,7 +8,10 @@ PATH = 'content'
 
 TIMEZONE = 'America/New_York'
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = 'en_US'
+
+# Get the year to place in the copyright notice.
+CURRENT_YEAR = time.strftime("%Y")
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
